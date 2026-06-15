b2f9219050f4ee3fdea660bf20c0dfa41d3c3ff2
molka-m <molkahchaichi30@gmail.com>
2026-06-15 20:20:33 +0100

    update logo and start project

--- src/app/layouts/full/vertical/sidebar/branding.component.ts
+++ src/app/layouts/full/vertical/sidebar/branding.component.ts
@@ -7,17 +7,19 @@ import { CoreService } from 'src/app/services/core.service';
   template: `
     <a href="/" class="logodark">
       <img
-        src="./assets/images/logos/dark-logo.svg"
+        src="./assets/images/BE THE ONE LOGO PNG 5.png"
         class="align-middle m-2"
         alt="logo"
+        style="max-width: 150px; height: auto;"
       />
     </a>
 
     <a href="/" class="logolight">
       <img
-        src="./assets/images/logos/light-logo.svg"
+        src="./assets/images/BE THE ONE LOGO PNG 5.png"
         class="align-middle m-2"
         alt="logo"
+        style="max-width: 150px; height: auto;"
       />
     </a>
   `,
